053a64b015032b403a15cfdb874dd22cf51cade9
David Malmström <syntex@outlook.com>
2016-08-09 08:46:04 +0200

    Init

---
+++ SC1605 AngularJS/js/main.js
@@ -0,0 +1,48 @@
+﻿"use strict";
+var app = angular.module("AJS", []);
+
+var item = {
+    name: "Coca-Cola Zero",
+    price: 12.90,
+    description:"Cola utan socker och kalorier, innehåller i princip ingenting."
+}
+
+app.controller("StoreController", function($scope, $interval) {
+    $scope.product = item;
+    $scope.isVisible = false;
+    $scope.header = "";
+    $scope.menu = "home";
+    $scope.timer = 10;
+
+    $scope.showText = function() {
+        $scope.isVisible = !$scope.isVisible;
+    };
+
+    $scope.reverseText = function (text) {
+        return text.split("").reverse().join("");
+    };
+
+    $scope.showPage = function(page) {
+        $scope.menu = page;
+    };
+
+    var decreaseCountDown = function() {
+        if ($scope.timer <= 1) {
+            $scope.stopCountDown();
+        }
+        $scope.timer--;
+    };
+
+    var intervalTimer;
+
+    $scope.startCountDown = function() {
+        $scope.timer = 10;
+        intervalTimer = $interval(decreaseCountDown, 1000);
+    };
+
+    $scope.stopCountDown = function() {
+        $interval.cancel(intervalTimer);
+        intervalTimer = undefined;
+    };
+
+});
